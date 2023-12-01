49c2d7a62a8d8e0d6228850f0284fd3db8d11714
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-02 00:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+	var body = document.querySelector("body");
+
+	if (body.hasChildNodes()) {
+		body.innerHTML = "";
+	}
+
+	var textNode = document.createTextNode("DOM load success");
+
+	body.appendChild(textNode);
+});
